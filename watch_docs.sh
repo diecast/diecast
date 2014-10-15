e40200c15e9c13fd6ce69a9b086aa63a9275ad83
Jorge Israel Peña <jorge.israel.p@gmail.com>
2014-10-15 16:01:56 -0700

    better doc watch script

--- watch_docs.sh
+++ watch_docs.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+trap 'exit' INT TERM
+trap 'kill 0' EXIT
+
+# web server
+mkdir -p target/doc
+cd target/doc
+python2 -m SimpleHTTPServer 4040 &
+
+# regen docs
+cd ../..
+
 while true; do
-  find src -iname \*rs |\
-    inotifywait -e close_write,delete_self --fromfile - && cargo doc;
+  inotifywait -e close_write,delete_self -r src/ && cargo doc;
 done
+
