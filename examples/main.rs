2dc8476a59b20dcb6b2f895f956e84be38bc97a3
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-02-05 20:54:11 -0800

    format main example

--- examples/main.rs
+++ examples/main.rs
@@ -9,44 +9,45 @@ extern crate glob;
 extern crate regex;
 
 use diecast::{
-  Site,
-  Rule,
-  Compiler,
-  Chain,
-  Item,
-  Dependencies,
+    Site,
+    Rule,
+    Compiler,
+    Chain,
+    Item,
+    Dependencies,
 };
 
 use diecast::router;
 use diecast::compiler::{self, TomlMetadata};
 
 fn main() {
-  let content_compiler =
-    Compiler::new(
-      Chain::new()
-        .link(compiler::read)
-        .link(compiler::parse_toml)
-        .link(|&: item: &mut Item, _deps: Option<Dependencies>| {
-          if let Some(&TomlMetadata(ref meta)) = item.data.get::<TomlMetadata>() {
-            println!("meta:\n{}", meta);
-          }
-          println!("body:\n{:?}", item.body);
-        })
-        .link(router::SetExtension::new("html"))
-        .link(|&: item: &mut Item, _deps: Option<Dependencies>| {
-          println!("routed {} → {}",
-                   item.from.clone().unwrap().display(),
-                   item.to.clone().unwrap().display());
-        })
-        .build());
+    let content_compiler =
+        Compiler::new(
+            Chain::new()
+            .link(compiler::read)
+            .link(compiler::parse_toml)
+            .link(|&: item: &mut Item, _deps: Option<Dependencies>| {
+                if let Some(&TomlMetadata(ref meta)) = item.data.get::<TomlMetadata>() {
+                    println!("meta:\n{}", meta);
+                }
 
-  let posts =
-    Rule::new("posts")
-      .compiler(content_compiler.clone());
+                println!("body:\n{:?}", item.body);
+            })
+            .link(router::SetExtension::new("html"))
+            .link(|&: item: &mut Item, _deps: Option<Dependencies>| {
+                println!("routed {} → {}",
+                         item.from.clone().unwrap().display(),
+                         item.to.clone().unwrap().display());
+            })
+            .build());
 
-  let site =
-    Site::new(Path::new("tests/fixtures/input"), Path::new("output"))
-      .matching(glob::Pattern::new("posts/*.md").unwrap(), posts);
+    let posts =
+        Rule::new("posts")
+        .compiler(content_compiler.clone());
 
-  site.build();
+    let site =
+        Site::new(Path::new("tests/fixtures/input"), Path::new("output"))
+        .matching(glob::Pattern::new("posts/*.md").unwrap(), posts);
+
+    site.build();
 }
